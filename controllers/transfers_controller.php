21ecac9f40e7bafd1c45436286d1888f3b60fa24
forpaul <socseti48@gmail.com>
2019-02-08 22:09:12 +0700

    Update transfers_controller.php

--- controllers/transfers_controller.php
+++ controllers/transfers_controller.php
@@ -3,13 +3,13 @@
 
     class getTransfer{
         public function DataTransfer(){
-            if(isset($_POST['transfer']))
+            if(isset($_POST['transfer'])) //get data from transfer form
                 $fromuser = $_POST['fromuser'];
                 $howmany = $_POST['howmany'];
                 $msg = $_POST['message'];
                 $touser = $_POST['touser'];
 
-                $setdata = new treatmentTransfer();
+                $setdata = new treatmentTransfer();  //generate new object for transfer
                 $setdata->treat($fromuser, $howmany, $msg, $touser);
         }
     }
